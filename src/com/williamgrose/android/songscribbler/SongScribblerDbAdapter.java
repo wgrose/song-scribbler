32bb1bc8967170f2cd403f6cb79ad838efa8a673
William Grose <williamgrose@gmail.com>
2008-11-09 23:06:10 -0500

    Got rid of copyrights....

--- src/com/williamgrose/android/songscribbler/SongScribblerDbAdapter.java
+++ src/com/williamgrose/android/songscribbler/SongScribblerDbAdapter.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package com.williamgrose.android.songscribbler;
 
 import android.content.ContentValues;
@@ -24,16 +8,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-/**
- * Simple notes database access helper class. Defines the basic CRUD operations
- * for the notepad example, and gives the ability to list all notes as well as
- * retrieve or modify a specific note.
- *
- * This has been improved from the first version of this tutorial through the
- * addition of better error handling and also using returning a Cursor instead
- * of using a collection of inner classes (which is less scalable and not
- * recommended).
- */
 public class SongScribblerDbAdapter {
 
     public static final String KEY_TITLE = "title";
